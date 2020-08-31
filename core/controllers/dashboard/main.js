620361ca8d2cb0a767f21353ee740a9de4391148
Jose Luis <47016453+JLRG806@users.noreply.github.com>
2020-08-31 15:34:23 -0600

    update

--- core/controllers/dashboard/main.js
+++ core/controllers/dashboard/main.js
@@ -1,9 +1,10 @@
+const API = '../../core/api/dashboard/productos.php?action=';
 // Función para graficar la cantidad de productos por categoría.
 function graficaCategorias()
 {
     $.ajax({
         dataType: 'json',
-        url: API_PRODUCTOS + 'cantidadProductosCategoria',
+        url: API + 'cantidadProductosCategoria',
         data: null
     })
     .done(function( response ) {
@@ -21,7 +22,7 @@ function graficaCategorias()
             // Se llama a la función que genera y muestra una gráfica de barras. Se encuentra en el archivo components.js
             barGraph( 'chart', categorias, cantidad, 'Cantidad de productos', 'Cantidad de productos por categoría' );
         } else {
-            $( '#chart' ).remove();
+            $( '#chart1' ).remove();
         }
     })
     .fail(function( jqXHR ) {
@@ -32,4 +33,76 @@ function graficaCategorias()
             console.log( jqXHR.status + ' ' + jqXHR.statusText );
         }
     });
+}
+
+function graficaClientes()
+{
+    $.ajax({
+        dataType: 'json',
+        url: API + 'cantidadClientesRegistrados',
+        data: null
+    })
+    .done(function( response ) {
+        // Se comprueba si la API ha retornado datos, de lo contrario se remueve la etiqueta canvas asignada para la gráfica.
+        if ( response.status ) {
+            // Se declaran los arreglos para guardar los datos por gráficar.
+            let tipo = [];
+            let cantidad = [];
+            // Se recorre el conjunto de registros devuelto por la API (dataset) fila por fila a través del objeto row.
+            response.dataset.forEach(function( row ) {
+                // Se asignan los datos a los arreglos.
+                tipo.push( row.tipo_cliente );
+                cantidad.push( row.cantidad );
+            });
+            // Se llama a la función que genera y muestra una gráfica de barras. Se encuentra en el archivo components.js
+            barGraph( 'chart', categorias, cantidad, 'Cantidad de clientes', 'Cantidad de clientes por tipo de cliente' );
+        } else {
+            $( '#chart2' ).remove();
+        }
+    })
+    .fail(function( jqXHR ) {
+        // Se verifica si la API ha respondido para mostrar la respuesta, de lo contrario se presenta el estado de la petición.
+        if ( jqXHR.status == 200 ) {
+            console.log( jqXHR.responseText );
+        } else {
+            console.log( jqXHR.status + ' ' + jqXHR.statusText );
+        }
+    });
+
+}
+
+function graficaPedidos()
+{
+    $.ajax({
+        dataType: 'json',
+        url: API + 'cantidadPedidosEntregados',
+        data: null
+    })
+    .done(function( response ) {
+        // Se comprueba si la API ha retornado datos, de lo contrario se remueve la etiqueta canvas asignada para la gráfica.
+        if ( response.status ) {
+            // Se declaran los arreglos para guardar los datos por gráficar.
+            let Entregas = [];
+            let cantidad = [];
+            // Se recorre el conjunto de registros devuelto por la API (dataset) fila por fila a través del objeto row.
+            response.dataset.forEach(function( row ) {
+                // Se asignan los datos a los arreglos.
+                categorias.push( row.entregas );
+                cantidad.push( row.cantidad );
+            });
+            // Se llama a la función que genera y muestra una gráfica de barras. Se encuentra en el archivo components.js
+            barGraph( 'chart', categorias, cantidad, 'Numero de pedidos', 'Numero de pedidos Entregados ' );
+        } else {
+            $( '#chart3' ).remove();
+        }
+    })
+    .fail(function( jqXHR ) {
+        // Se verifica si la API ha respondido para mostrar la respuesta, de lo contrario se presenta el estado de la petición.
+        if ( jqXHR.status == 200 ) {
+            console.log( jqXHR.responseText );
+        } else {
+            console.log( jqXHR.status + ' ' + jqXHR.statusText );
+        }
+    });
+    
 }
